572511745287f3fb6713de5230ab2e54f43521da
Daniela Ruiz <daru015@gmail.com>
2018-07-05 17:27:02 +0200

    body example

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,23 +6,35 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            showMenu: false
-        }
+            showMenu: false,
+        };
     }
 
     toggleMenu = () => {
         this.setState({showMenu: !this.state.showMenu});
     };
 
-    render(){
+    render() {
         return (<div>
-                    <Header showMenu={this.state.showMenu} toggleMenu={this.toggleMenu} />
-                    <div>
-                        <h3>Body</h3>
+            <Header showMenu={this.state.showMenu} toggleMenu={this.toggleMenu}/>
+            <div className="card">
+                <div className="card-body">
+                    <div className="jumbotron">
+                        <h1 className="display-4">Hello, world!</h1>
+                        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling
+                            extra
+                            attention to featured content or information.</p>
+                        <hr className="my-4"/>
+                        <p>It uses utility classes for typography and spacing to space content out within the larger
+                            container.</p>
+                        <p className="lead">
+                            <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                        </p>
                     </div>
-                </div>)
+                </div>
+            </div>
+        </div>);
     }
 }
-
 
 export default App;
