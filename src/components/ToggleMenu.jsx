f4710abb24a4ebb50c06c219b329df9b417909dd
Daniela Ruiz <daru015@gmail.com>
2018-07-05 17:17:04 +0200

    toggle menu

---
+++ src/components/ToggleMenu.jsx
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+//Components
+import MenuItem from './MenuItem';
+import Icon from './Icon';
+
+export class CustomToggleMenu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showMenu: false,
+        };
+    }
+
+    toggleMenu = () => {
+        this.setState({showMenu: !this.state.showMenu});
+    };
+
+    render() {
+        const {showMenu} = this.state;
+        const {children} = this.props;
+        let optionsList = (React.Children.count(children) > 1) ? children.map((x, i) => <MenuItem key={`toggle-menu-item-${i}`} label={x} />):
+            <MenuItem key={`toggle-menu-item-${0}`} label={children}/>;
+        return (<div className={`dropdown ${showMenu ? 'show' : ''}`}>
+                    <button className="btn btn-secondary" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onClick={this.toggleMenu}>
+                       <Icon />
+                    </button>
+                    <div className={`dropdown-menu dropdown-menu-right ${showMenu ? 'show' : ''}`} aria-labelledby="dropdownMenuButton">
+                        {showMenu && optionsList}
+                    </div>
+        </div>);
+    }
+}
+
+CustomToggleMenu.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export default CustomToggleMenu;
