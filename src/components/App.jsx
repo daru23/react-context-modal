d120c5470ab2e3312bdc59fd658ba4596782d876
Daniela Ruiz <daru015@gmail.com>
2018-07-05 12:54:35 +0200

    main component

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () =>
+    <div>
+        <div>
+            <button onClick={}>
+                button
+            </button>
+        </div>
+    </div>;
+
+export default App;
