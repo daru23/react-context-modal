dfc62df64e5cb89ff8a9d8e9a443538cf0a4e19f
Daniela Ruiz <daru015@gmail.com>
2018-07-05 17:17:41 +0200

    App refacor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Header from './Header';
 
-const App = () =>
-    <div>
-        <div>
-            <button onClick={() => {}}>
-                button
-            </button>
-        </div>
-    </div>;
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            showMenu: false
+        }
+    }
+
+    toggleMenu = () => {
+        this.setState({showMenu: !this.state.showMenu});
+    };
+
+    render(){
+        return (<div>
+                    <Header showMenu={this.state.showMenu} toggleMenu={this.toggleMenu} />
+                    <div>
+                        <h3>Body</h3>
+                    </div>
+                </div>)
+    }
+}
+
 
 export default App;
