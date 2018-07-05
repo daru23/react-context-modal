ac18bf089275387a948a19e2dff846683cca5a6d
Daniela Ruiz <daru015@gmail.com>
2018-07-05 17:16:24 +0200

    icon for menu toggle

---
+++ src/components/Icon.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Icon = () =>
+    <svg id="i-ellipsis-horizontal" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2">
+        <circle cx="7" cy="16" r="2" />
+        <circle cx="16" cy="16" r="2" />
+        <circle cx="25" cy="16" r="2" />
+    </svg>
+export default Icon;
