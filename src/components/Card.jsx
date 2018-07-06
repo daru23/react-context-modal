9d2c9315167b3709b2088f50b14e07f0250492d0
Daniela Ruiz <daru015@gmail.com>
2018-07-06 17:33:08 +0200

    refactor Cards header removed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,9 +3,8 @@ import React from 'react';
 import MouseTracker from './MouseTracker';
 // Utils
 import {toTitleCase} from '../utils/utils';
-export const Card = ({title}) =>
+export const Card = () =>
     <div className="card border-light mb-3" style={{height: '300px'}}>
-        <div className="card-header">{toTitleCase(title)}</div>
         <div className="card-body">
         <MouseTracker />
         </div>
