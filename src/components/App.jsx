e5572b6e46df0d9defae0ce3e55c91aafd292b41
Daniela Ruiz <daru015@gmail.com>
2018-07-06 10:45:21 +0200

    refactor simplifying App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,10 @@
 import React, {Component} from 'react';
 import Header from './Header';
 
-class App extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            showMenu: false,
-        };
-    }
-
-    toggleMenu = () => {
-        this.setState({showMenu: !this.state.showMenu});
-    };
-
+export class App extends Component {
     render() {
-        return (<div>
-            <Header showMenu={this.state.showMenu} toggleMenu={this.toggleMenu}/>
+        return (<div id="app-component">
+            <Header />
             <div className="card">
                 <div className="card-body">
                     <div className="jumbotron">
