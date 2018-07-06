523fb0f6ba23ea00f134ba9be380dc9754437e8d
Daniela Ruiz <daru015@gmail.com>
2018-07-06 15:31:50 +0200

    upadting texts

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -7,14 +7,11 @@ export const Body = () =>
         <div className="card-body">
             <div className="jumbotron">
                 <h1 className="display-4">Hello, world!</h1>
-                <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling
-                    extra
-                    attention to featured content or information.</p>
+                <p className="lead">This is a simple React Application.</p>
                 <hr className="my-4"/>
                 <Card title={"Move your mouse inside the box bellow..."}/>
                 <hr className="my-4"/>
-                <p>It uses utility classes for typography and spacing to space content out within the larger
-                    container.</p>
+                <p>I will test some patters and paradigms in this project.</p>
                 <p className="lead">
                     <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
                 </p>
