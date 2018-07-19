c51a9ca31e0b676dc02f3a5e14b7040c93397eab
Daniela Ruiz <daru015@gmail.com>
2018-07-19 15:48:01 +0200

    refactor home page and responsive nav bar

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,7 +5,11 @@ export const Body = () =>
     <div className="jumbotron" style={{height: "100vh"}}>
         <p className="display-4">This is a simple React Application.</p>
         <p className="lead">Here I will implement some example and React Paradigms.</p>
-        <img className="mx-auto" src={"/assets/React-Logo.svg.png"} />
+        <div className="container">
+            <div className="row justify-content-md-center">
+                <img className="rotate" src={"/assets/React-Logo.svg.png"} />
+            </div>
+        </div>
         <hr className="my-4"/>
         <p>I will test some patters and paradigms in this project.</p>
         <p className="lead">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,19 @@ import {Link} from 'react-router-dom';
 import ToggleMenu from './ToggleMenu';
 
 export const Header = ({}) =>
-    <nav className="navbar navbar-dark bg-dark justify-content-end">
-        <Link className="navbar-brand" to="/">React Examples</Link>
-        <Link className="navbar-brand" to="/mouse-tracker/">Mouse Tracker</Link>
-        <Link className="navbar-brand" to="/whiteboard/">Whiteboard</Link>
-        <Link className="navbar-brand" to="/rock-paper-scissors/">Rock Paper Scissors</Link>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <Link className="navbar-brand" to="/">React Portafolio</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="navbar-collapse collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item"><Link className="nav-link" to="/mouse-tracker/">Mouse Tracker</Link></li>
+                <li className="nav-item"><Link className="nav-link" to="/whiteboard/">Whiteboard</Link></li>
+                <li className="nav-item"><Link className="nav-link" to="/rock-paper-scissors/">Rock Paper Scissors</Link></li>
+            </ul>
+        </div>
         <ToggleMenu>
             <div>Edit</div>
             <div>Logout</div>
